90979e74f33783f2ca71f8b412ba2bf54c8383b7
Scott Westover <scottwestover2006@gmail.com>
2023-08-21 21:40:35 -0400

    [GH-2] finished icicle animation
    Signed-off-by: Scott Westover <scottwestover2006@gmail.com>

--- phaser-3/3.60/animation-completion-events/src/main.js
+++ phaser-3/3.60/animation-completion-events/src/main.js
@@ -16,6 +16,7 @@ class MainScene extends Phaser.Scene {
       frameWidth: 32,
       frameHeight: 48,
     });
+    this.load.image('pic', 'https://labs.phaser.io/assets/pics/kris-jovo.jpg');
   }
 
   create() {
@@ -40,36 +41,51 @@ class MainScene extends Phaser.Scene {
       .setScale(2)
       .setAlpha(0.5);
 
-    const cropWidth = this.#icicle.displayWidth;
-    const cropHeight = this.#icicle.displayHeight;
-    this.offset = this.#icicle.getTopLeft();
+    this.#icicle.once(Phaser.Animations.Events.ANIMATION_COMPLETE, () => {
+      console.log('animation completed');
+    });
+    this.#icicle.once(
+      Phaser.Animations.Events.ANIMATION_COMPLETE_KEY +
+        this.#iceAttackAnimationName,
+      () => {
+        console.log('attack animation completed');
+        const cropWidth = this.#icicle.displayWidth * 0.5;
+        const cropHeight = this.#icicle.displayHeight * 0.5;
+        this.#icicle.setCrop(0, cropHeight / 2, cropWidth, cropHeight / 2);
 
-    //icicle.play(this.#iceAttackAnimationName);
+        const icicleBase = this.add
+          .sprite(this.scale.width / 2, 0, this.#iceSpriteSheetAssetName, 6)
+          .setOrigin(0.5, 0)
+          .setScale(2)
+          .setAlpha(0.5);
+        icicleBase.setCrop(0, 0, cropWidth, cropHeight / 2);
 
-    this.#icicle.setCrop(0, 0, cropWidth / 2, cropHeight);
-
-    this.graphics = this.add.graphics();
-    this.graphics.clear();
-    this.graphics.lineStyle(1, 0x00ff00);
-    this.graphics.strokeRect(
-      this.#icicle.x - this.#icicle.displayWidth / 2,
-      this.#icicle.displayHeight / 2,
-      this.#icicle.displayWidth,
-      this.#icicle.displayHeight / 2
+        const startYPos = this.#icicle.y;
+        const endYPos = startYPos + 500;
+        this.tweens.add({
+          delay: 0,
+          duration: 1200,
+          y: {
+            from: startYPos,
+            start: startYPos,
+            to: endYPos,
+          },
+          targets: this.#icicle,
+          onComplete: () => {
+            this.#icicle.play(this.#iceBreakAnimationName);
+          },
+        });
+      }
+    );
+    this.#icicle.once(
+      Phaser.Animations.Events.ANIMATION_COMPLETE_KEY +
+        this.#iceBreakAnimationName,
+      () => {
+        this.#icicle.setAlpha(0);
+      }
     );
 
-    // overlay.fillStyle(0x000000, 0.8).fillRect(0, 0, 800, 600);
-    // const maskGraphics = this.make.graphics();
-    // maskGraphics.fillStyle(0xffffff);
-    // maskGraphics.fillRect(icicle.x, icicle.y, 256, 256);
-    // const mask = new Phaser.Display.Masks.BitmapMask(this, maskGraphics);
-    // mask.invertAlpha = true;
-    // overlay.setMask(mask);
-    // https://labs.phaser.io/edit.html?src=src/textures\crop%20texture%20image.js
-  }
-
-  update() {
-    //this.#icicle.y += 1;
+    this.#icicle.play(this.#iceAttackAnimationName);
   }
 }
 
