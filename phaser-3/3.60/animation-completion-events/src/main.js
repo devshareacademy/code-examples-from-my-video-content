64b7fde36e00da428a28b7c2452fee08605c2bbb
Scott Westover <scottwestover2006@gmail.com>
2023-08-21 21:55:53 -0400

    [GH-2] added basic lightning animation
    Signed-off-by: Scott Westover <scottwestover2006@gmail.com>

--- phaser-3/3.60/animation-completion-events/src/main.js
+++ phaser-3/3.60/animation-completion-events/src/main.js
@@ -16,7 +16,6 @@ class MainScene extends Phaser.Scene {
       frameWidth: 32,
       frameHeight: 48,
     });
-    this.load.image('pic', 'https://labs.phaser.io/assets/pics/kris-jovo.jpg');
   }
 
   create() {
@@ -38,8 +37,7 @@ class MainScene extends Phaser.Scene {
     this.#icicle = this.add
       .sprite(this.scale.width / 2, 0, this.#iceSpriteSheetAssetName, 6)
       .setOrigin(0.5, 0)
-      .setScale(2)
-      .setAlpha(0.5);
+      .setScale(2);
 
     this.#icicle.once(Phaser.Animations.Events.ANIMATION_COMPLETE, () => {
       console.log('animation completed');
@@ -89,16 +87,65 @@ class MainScene extends Phaser.Scene {
   }
 }
 
+class TestScene extends Phaser.Scene {
+  #spriteSheetAssetName = 'lightning';
+
+  constructor() {
+    super(TestScene.name);
+  }
+
+  preload() {
+    this.load.spritesheet(this.#spriteSheetAssetName, 'assets/lightning.png', {
+      frameWidth: 32,
+      frameHeight: 96,
+    });
+  }
+
+  create() {
+    this.anims.create({
+      key: this.#spriteSheetAssetName,
+      frames: this.anims.generateFrameNumbers(this.#spriteSheetAssetName),
+      frameRate: 4,
+    });
+
+    const lightning = this.add
+      .sprite(this.scale.width / 2, 0, this.#spriteSheetAssetName, 0)
+      .setOrigin(0.5, 0)
+      .setScale(5);
+
+    lightning.once(Phaser.Animations.Events.ANIMATION_COMPLETE, () => {
+      console.log('animation completed');
+    });
+
+    let counter = 0;
+    lightning.on(
+      Phaser.Animations.Events.ANIMATION_COMPLETE_KEY +
+        this.#spriteSheetAssetName,
+      () => {
+        counter += 1;
+        console.log(`lightning strike: ${counter}`);
+        lightning.setAlpha(0).setFrame(0).setX(Phaser.Math.Between(100, 700));
+        this.time.delayedCall(800, () => {
+          lightning.setAlpha(1);
+          lightning.play(this.#spriteSheetAssetName);
+        });
+      }
+    );
+
+    lightning.play(this.#spriteSheetAssetName);
+  }
+}
+
 const gameConfig = {
   type: Phaser.CANVAS,
   pixelArt: true,
   scale: {
     parent: 'game-container',
     width: 800,
-    height: 600,
+    height: 480,
   },
   backgroundColor: '#5c5b5b',
-  scene: [MainScene],
+  scene: [TestScene, MainScene],
 };
 
 const game = new Phaser.Game(gameConfig);
