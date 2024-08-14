e95b622957f57cdbd8b3a271d77f589ab689f2e6
Scott Westover <scottwestover2006@gmail.com>
2024-08-13 21:37:44 -0400

    temp changes for minimap demo
    Signed-off-by: Scott Westover <scottwestover2006@gmail.com>

--- phaser-3/3.80/metroid-minimap-demo/src/map-scene.js
+++ phaser-3/3.80/metroid-minimap-demo/src/map-scene.js
@@ -10,6 +10,8 @@ export class MapScene extends Phaser.Scene {
   /** @type {boolean} */
   #mainMapShown;
   /** @type {Phaser.Tilemaps.Tilemap} */
+  #map;
+  /** @type {Phaser.Tilemaps.Tilemap} */
   #levelTileMap;
   /** @type {number} */
   #levelRoomWidth;
@@ -55,107 +57,37 @@ export class MapScene extends Phaser.Scene {
    * @returns {void}
    */
   create() {
+    // create scene background
     this.add.rectangle(0, 0, this.scale.width, this.scale.height, 0x000000).setOrigin(0);
-    const map = this.add.tilemap('map');
-    const tileset = map.addTilesetImage('map-Sheet', 'minimapTileset');
-    this.#mapTilemapLayer = map.createLayer('map', tileset);
-    const iconTileset = map.addTilesetImage('objects', 'icons');
-    this.#itemTilemapLayer = map.createLayer('items', iconTileset);
+    // create map based on Tiled map data
+    this.#map = this.add.tilemap('map');
+    const tileset = this.#map.addTilesetImage('map-Sheet', 'minimapTileset');
+    this.#mapTilemapLayer = this.#map.createLayer('map', tileset);
+    const iconTileset = this.#map.addTilesetImage('objects', 'icons');
+    this.#itemTilemapLayer = this.#map.createLayer('items', iconTileset);
+    // update map location to be centered in scene
     const mapX = this.scale.width / 2 - this.#mapTilemapLayer.width / 2;
     const mapY = this.scale.height / 2 - this.#mapTilemapLayer.height / 2;
     this.#mapTilemapLayer.setPosition(mapX, mapY);
     this.#itemTilemapLayer.setPosition(mapX, mapY);
+    // update scene to be in the minimap format
     this.toggleMap();
 
     // calculate dimensions of map based on actual level sizes
-    this.#levelRoomHeight = this.#levelTileMap.heightInPixels / map.height;
-    this.#levelRoomWidth = this.#levelTileMap.widthInPixels / map.width;
-    this.#mapRoomHeight = map.tileHeight;
-    this.#mapRoomWidth = map.tileWidth;
-    for (let x = 0; x < map.width; x += 1) {
-      for (let y = 0; y < map.height; y += 1) {
-        const tile = this.#mapTilemapLayer.getTileAt(x, y);
-        if (tile === null) {
-          continue;
-        }
-        tile.setVisible(false);
-        this.#roomsVisited[this.#createRoomVisitedKey(x, y)] = false;
+    this.#levelRoomHeight = this.#levelTileMap.heightInPixels / this.#map.height;
+    this.#levelRoomWidth = this.#levelTileMap.widthInPixels / this.#map.width;
+    this.#mapRoomHeight = this.#map.tileHeight;
+    this.#mapRoomWidth = this.#map.tileWidth;
 
-        // handle items
-        const itemTile = this.#itemTilemapLayer.getTileAt(x, y);
-        if (itemTile === null) {
-          continue;
-        }
-        itemTile.setVisible(false);
-        this.#items[this.#createRoomVisitedKey(x, y)] = false;
-      }
-    }
+    this.#createRoomVisitedData();
+    this.#createPlayerCursor();
 
-    this.#playerCursor = this.add.rectangle(0, 0, this.#mapRoomWidth, this.#mapRoomHeight, 0xff0000).setOrigin(0);
-    this.tweens.add({
-      targets: this.#playerCursor,
-      alpha: 0,
-      yoyo: true,
-      repeat: -1,
-      duration: 800,
-      ease: Phaser.Math.Easing.Sine.InOut,
-    });
-
-    this.registry.events.on('changedata', (parent, key, data) => {
-      if (key !== 'playerPosition' && key !== 'mapPickedUp' && key !== 'itemsPickedUp') {
-        return;
-      }
-      if (key === 'mapPickedUp') {
-        this.#showMap();
-        return;
-      }
-      if (key === 'itemsPickedUp') {
-        const itemTile = this.#itemTilemapLayer.getTileAtWorldXY(this.#playerCursor.x, this.#playerCursor.y);
-        itemTile.index = 25;
-        return;
-      }
-      if (this.#mainMapShown) {
-        return;
-      }
-
-      // update the camera position based on the players location, so that the center point of the viewport
-      // will be the players room location
-      // to calculate the location, we need to take the players x and y position on the original map and
-      // calculate the same x and y value on the minimap location
-      const x = Math.floor(data.x / this.#levelRoomWidth);
-      const y = Math.floor(data.y / this.#levelRoomHeight);
-
-      const playerX = mapX + x * this.#mapRoomWidth;
-      const playerY = mapY + y * this.#mapRoomHeight;
-      const centeredX = playerX - this.#mapRoomWidth;
-      const centeredY = playerY - this.#mapRoomHeight;
-
-      if (!this.#roomsVisited[this.#createRoomVisitedKey(x, y)]) {
-        this.#roomsVisited[this.#createRoomVisitedKey(x, y)] = true;
-        const tile = this.#mapTilemapLayer.getTileAt(x, y);
-        if (tile === null) {
-          return;
-        }
-        tile.setVisible(true);
-        tile.index = tile.index + 11;
-      }
-
-      // handle item
-      if (!this.#items[this.#createRoomVisitedKey(x, y)]) {
-        this.#items[this.#createRoomVisitedKey(x, y)] = true;
-        const tile = this.#itemTilemapLayer.getTileAt(x, y);
-        if (tile === null) {
-          return;
-        }
-        tile.setVisible(true);
-      }
-
-      this.#playerCursor.setPosition(centeredX + this.#mapRoomWidth, centeredY + this.#mapRoomHeight);
-      this.cameras.main.setScroll(centeredX, centeredY);
-    });
+    // register event listener for shared data changes
+    this.registry.events.on('changedata', this.#handleRegistryDataUpdated);
   }
 
   /**
+   * Toggle Phaser Scene between full scene view (display full map) and minimap view in the main Phaser game scene.
    * @returns {void}
    */
   toggleMap() {
@@ -174,6 +106,109 @@ export class MapScene extends Phaser.Scene {
   }
 
   /**
+   * Used to populate to objects that are used for tracking which rooms and items should be
+   * revealed to the player on the map. By default, no room and item data will be revealed
+   * until the player enters that room (on the minimap).
+   */
+  #createRoomVisitedData() {
+    for (let x = 0; x < this.#map.width; x += 1) {
+      for (let y = 0; y < this.#map.height; y += 1) {
+        const tile = this.#mapTilemapLayer.getTileAt(x, y);
+        if (tile === null) {
+          continue;
+        }
+        tile.setVisible(false);
+        this.#roomsVisited[this.#createRoomVisitedKey(x, y)] = false;
+
+        // handle items
+        const itemTile = this.#itemTilemapLayer.getTileAt(x, y);
+        if (itemTile === null) {
+          continue;
+        }
+        itemTile.setVisible(false);
+        this.#items[this.#createRoomVisitedKey(x, y)] = false;
+      }
+    }
+  }
+
+  /**
+   * Updates map data and view based on the shared data registry in the Phaser Game.
+   * When the player moves in the main Phaser Scene, the players updated position is detected here
+   * which will have the camera scroll and move to show the current room to the player.
+   *
+   * When an item is picked up in the game scene, the picked up item is detected here and the
+   * map will update to reflect the item has been picked up by the player.
+   *
+   * Lastly, when the player clicks the `p` key to simulate map data being made to the player will
+   * trigger the map being revealed to the player.
+   */
+  #handleRegistryDataUpdated(parent, key, data) {
+    if (key !== 'playerPosition' && key !== 'mapPickedUp' && key !== 'itemsPickedUp') {
+      return;
+    }
+    if (key === 'mapPickedUp') {
+      this.#showMap();
+      return;
+    }
+    if (key === 'itemsPickedUp') {
+      const itemTile = this.#itemTilemapLayer.getTileAtWorldXY(this.#playerCursor.x, this.#playerCursor.y);
+      itemTile.index = 25;
+      return;
+    }
+    if (this.#mainMapShown) {
+      return;
+    }
+
+    // update the camera position based on the players location, so that the center point of the viewport
+    // will be the players room location
+    // to calculate the location, we need to take the players x and y position on the original map and
+    // calculate the same x and y value on the minimap location
+    const x = Math.floor(data.x / this.#levelRoomWidth);
+    const y = Math.floor(data.y / this.#levelRoomHeight);
+
+    const playerX = this.#mapTilemapLayer.x + x * this.#mapRoomWidth;
+    const playerY = this.#mapTilemapLayer.y + y * this.#mapRoomHeight;
+    const centeredX = playerX - this.#mapRoomWidth;
+    const centeredY = playerY - this.#mapRoomHeight;
+
+    if (!this.#roomsVisited[this.#createRoomVisitedKey(x, y)]) {
+      this.#roomsVisited[this.#createRoomVisitedKey(x, y)] = true;
+      const tile = this.#mapTilemapLayer.getTileAt(x, y);
+      if (tile === null) {
+        return;
+      }
+      tile.setVisible(true);
+      tile.index = tile.index + 11;
+    }
+
+    // handle item
+    if (!this.#items[this.#createRoomVisitedKey(x, y)]) {
+      this.#items[this.#createRoomVisitedKey(x, y)] = true;
+      const tile = this.#itemTilemapLayer.getTileAt(x, y);
+      if (tile === null) {
+        return;
+      }
+      tile.setVisible(true);
+    }
+
+    this.#playerCursor.setPosition(centeredX + this.#mapRoomWidth, centeredY + this.#mapRoomHeight);
+    this.cameras.main.setScroll(centeredX, centeredY);
+  }
+
+  #createPlayerCursor() {
+    this.#playerCursor = this.add.rectangle(0, 0, this.#mapRoomWidth, this.#mapRoomHeight, 0xff0000).setOrigin(0);
+    this.tweens.add({
+      targets: this.#playerCursor,
+      alpha: 0,
+      yoyo: true,
+      repeat: -1,
+      duration: 800,
+      ease: Phaser.Math.Easing.Sine.InOut,
+    });
+  }
+
+  /**
+   * Creates the unique key for tracking which rooms have been visited by the player.
    * @param {number} x
    * @param {number} y
    * @returns {string}
@@ -183,6 +218,9 @@ export class MapScene extends Phaser.Scene {
   }
 
   /**
+   * Updates all of the tiles in the Tilemap to now be visible, which simulates the player
+   * picking up a map in the game and so all rooms will be revealed but will show they have
+   * not been visited.
    * @returns {void}
    */
   #showMap() {
